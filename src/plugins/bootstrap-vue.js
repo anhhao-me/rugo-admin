a94eddd42668a6ede9139e98aecaaab9dc53f580
Anh Hao <hi@anhhao.me>
2021-03-02 09:32:08 +0700

    Update relative app.scss path

--- src/plugins/bootstrap-vue.js
+++ src/plugins/bootstrap-vue.js
@@ -3,6 +3,6 @@ import Vue from 'vue'
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-import '@/assets/scss/app.scss'
+import '../assets/scss/app.scss'
 
 Vue.use(BootstrapVue)
