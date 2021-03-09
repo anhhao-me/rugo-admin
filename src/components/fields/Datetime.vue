8d3d88804978878547076a67287962dabeea505b
Anh Hao <hi@anhhao.me>
2021-03-09 17:03:10 +0700

    Add datetime type

--- src/components/fields/Datetime.vue
+++ src/components/fields/Datetime.vue
@@ -1,11 +1,72 @@
 <template>
   <div>
+    <div v-if="mode === 'preview' && value">{{ formatedValue }}</div>
+    <div class="d-flex" v-if="!mode || mode === 'input'">
+      <b-form-datepicker
+        class="mr-1"
+        v-model="date"
+        @input="update"
+      />
+      <b-form-timepicker 
+        show-seconds
+        :hour12="false"
+        class="ml-1" v-model="time"
+        @input="update"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import moment from 'moment';
 
+export default {
+  allowModes: ['input', 'preview'],
+  props: ['value', 'mode'],
+  data(){
+    return {
+      date: null,
+      time: null
+    }
+  },
+  computed: {
+    formatedValue(){
+      return moment(this.value).format('YYYY-MM-DD HH:mm:ss');
+    }
+  },
+  methods: {
+    update(){
+      const d = moment(`${this.date} ${this.time}`, 'YYYY-MM-DD HH:mm:ss').toDate();
+      this.$emit('input', d);
+    },
+    breakValue(value){
+      if (!this.value){
+        return { date: null, time: null };
+      }
+
+      const d = moment(value);
+      const date = d.format('YYYY-MM-DD');
+      const time = d.format('HH:mm:ss');
+
+      return { date, time };
+    }
+  },
+  mounted(){
+    const { date, time } = this.breakValue(this.value);
+    if (this.date !== date)
+      this.date = date;
+    if (this.time !== time)
+      this.time = time;
+  },
+  watch: {
+    value(){
+      const { date, time } = this.breakValue(this.value);
+      if (this.date !== date)
+        this.date = date;
+      if (this.time !== time)
+        this.time = time;
+    }
+  }
 }
 </script>
 
